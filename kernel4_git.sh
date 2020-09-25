11d881279b14202edb11cd47b29cb9537f9eed32
elppans <elppansmk@gmail.com>
2020-09-25 14:31:25 -0300

    Update kernel4_git.sh

--- kernel4_git.sh
+++ kernel4_git.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+cp -fv /etc/apt/sources.list /etc/apt/sources.list.old
+#sed -i "/erlang/s/deb/#deb/g" /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d/*
-sed -i "/erlang/s/deb/#deb/g" /etc/apt/sources.list
+echo -e '
+deb http://old-releases.ubuntu.com/ubuntu precise main universe restricted multiverse
+deb http://old-releases.ubuntu.com/ubuntu precise-updates main universe restricted multiverse
+deb http://old-releases.ubuntu.com/ubuntu precise-security main universe restricted multiverse
+deb http://old-releases.ubuntu.com/ubuntu precise-proposed main universe restricted multiverse
+deb http://old-releases.ubuntu.com/ubuntu precise-backports main universe restricted multiverse
+' | tee /etc/apt/sources.list >> /dev/null
 apt-get update
 apt-get -y install git
 mkdir -p ~/kernel4
